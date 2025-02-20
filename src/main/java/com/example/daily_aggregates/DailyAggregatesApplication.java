3284979325b34eebb9f63878e5e2bb27cb0961b0
pauline <pauljdavi.t21@btinternet.com>
2025-02-20 14:56:33 +0000

    Added Spring Boot initalizer project setup

---
+++ src/main/java/com/example/daily_aggregates/DailyAggregatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.daily_aggregates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyAggregatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyAggregatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daily_aggregates/DailyAggregatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daily_aggregates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyAggregatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
