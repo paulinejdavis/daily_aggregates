0e2bf7e8091a00070852cc4b577e8a382d5ae00a
pauline <pauljdavi.t21@btinternet.com>
2025-02-22 15:18:12 +0000

    Added failing test for fetching Trade Summaries

--- src/test/java/com/example/daily_aggregates/TradeControllerTest.java
+++ src/test/java/com/example/daily_aggregates/TradeControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.daily_aggregates;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
@@ -18,9 +19,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Autowired
+    private TradeService tradeService;
+
     @Test
     void shouldReturnAggregatesAsJson() {
-        String url = "http://localhost:" + port + "/api/trades/aggregates";
+        String url = "http://localhost:" + port + "/trades/summary?filePath=src/test/resources/sample_trades.txt";
         Map<String, Map<String, DailySummary>> response = restTemplate.getForObject(url, Map.class);
 
         assertNotNull(response);
@@ -28,5 +32,29 @@ import static org.junit.jupiter.api.Assertions.*;
         assertTrue(response.containsKey("2025-01-20"));
         assertTrue(response.get("2025-01-20").containsKey("ABC"));
 
+    }
+
+    @Test
+    void shouldReturnTradeSummary() {
+        String filePath = "src/test/resources/sample_trades.txt";
+
+        try {
+            Map<String, Map<String, DailySummary>> summaries = tradeService.getDailySummaries(filePath);
+
+            assertFalse(summaries.isEmpty());
+            assertTrue(summaries.containsKey("2025-01-20"));
+            assertTrue(summaries.get("2025-01-20").containsKey("ABC"));
+
+            DailySummary abcSummary = summaries.get("2025-01-20").get("ABC");
+
+            assertEquals(100, abcSummary.openPrice());
+            assertEquals(105, abcSummary.closePrice());
+            assertEquals(105, abcSummary.highPrice());
+            assertEquals(100, abcSummary.lowPrice());
+            assertEquals(50000, abcSummary.totalVolume());
+
+        } catch (Exception e) {
+            fail("Exception occurred while fetching trade summaries: " + e.getMessage());
+        }
     }
 }
